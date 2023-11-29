a5af210854777b135640ce86136c0a5cadd7c768
leonelmaken <103751176+leonelmaken@users.noreply.github.com>
2023-11-29 19:17:52 +0100

    mise à jour

--- Back-end/Users/src/main/java/com/school/universitymanagementapp/Model/Student.java
+++ Back-end/Users/src/main/java/com/school/universitymanagementapp/Model/Student.java
@@ -56,19 +56,23 @@ public class Student extends User{
 		super();
 		// TODO Auto-generated constructor stub
 	}
-	public Student(String photocni, String relevebac, String releveproba, String actenaiss, String recu, String faculte,
-			String filiere, Specialite specialite, Niveau niveau, String dernierdiplom, String anneeObtent,
-			Double moyenne, String infojury, String matriculediplo, String delivrepar, Date datedeliv, String nompere,
-			String professpere, String nommere, String professmere, String nomurgent, Double numerourgent,
-			String villeurgent) {
+	
+    
+	public Student(String photocni, String relevebac, String releveproba, String actenaiss, String recu,
+			String premierchoix, String deuxiemechoix, String troisiemechoix, String specialite, Niveau niveau,
+			String dernierdiplom, String anneeObtent, Double moyenne, String infojury, String matriculediplo,
+			String delivrepar, Date datedeliv, String nompere, String professpere, String nommere, String professmere,
+			String nomtuteur, String emailtuteur, String professtuteur, String nomurgent, Double numerourgent,
+			String villeurgent, int etat, Double numerotransaction, Double codepreins, boolean sport, boolean art) {
 		super();
 		this.photocni = photocni;
 		this.relevebac = relevebac;
 		this.releveproba = releveproba;
 		this.actenaiss = actenaiss;
 		this.recu = recu;
-		this.faculte = faculte;
-		this.filiere = filiere;
+		this.premierchoix = premierchoix;
+		this.deuxiemechoix = deuxiemechoix;
+		this.troisiemechoix = troisiemechoix;
 		this.specialite = specialite;
 		this.niveau = niveau;
 		this.dernierdiplom = dernierdiplom;
@@ -77,238 +81,357 @@ public class Student extends User{
 		this.infojury = infojury;
 		this.matriculediplo = matriculediplo;
 		this.delivrepar = delivrepar;
-		this.Datedeliv = datedeliv;
+		Datedeliv = datedeliv;
 		this.nompere = nompere;
 		this.professpere = professpere;
 		this.nommere = nommere;
 		this.professmere = professmere;
+		this.nomtuteur = nomtuteur;
+		this.emailtuteur = emailtuteur;
+		this.professtuteur = professtuteur;
 		this.nomurgent = nomurgent;
 		this.numerourgent = numerourgent;
 		this.villeurgent = villeurgent;
-			}
-    
-	public String getNomtuteur() {
-		return nomtuteur;
-	}
-	public void setNomtuteur(String nomtuteur) {
-		this.nomtuteur = nomtuteur;
-	}
-	public String getEmailtuteur() {
-		return emailtuteur;
-	}
-	public void setEmailtuteur(String emailtuteur) {
-		this.emailtuteur = emailtuteur;
-	}
-	public String getProfesstuteur() {
-		return professtuteur;
-	}
-	public void setProfesstuteur(String professtuteur) {
-		this.professtuteur = professtuteur;
-	}
-	public Double getNumerotransaction() {
-		return numerotransaction;
-	}
-	public void setNumerotransaction(Double numerotransaction) {
+		this.etat = etat;
 		this.numerotransaction = numerotransaction;
-	}
-	public Double getCodepreins() {
-		return codepreins;
-	}
-	public void setCodepreins(Double codepreins) {
 		this.codepreins = codepreins;
-	}
-	public boolean isSport() {
-		return sport;
-	}
-	public void setSport(boolean sport) {
 		this.sport = sport;
-	}
-	public boolean isArt() {
-		return art;
-	}
-	public void setArt(boolean art) {
 		this.art = art;
 	}
-	public int getEtat() {
-		return etat;
-	}
-	public void setEtat(int etat) {
-		this.etat = etat;
 
-	}
+
 	public String getPhotocni() {
 		return photocni;
 	}
+
+
 	public void setPhotocni(String photocni) {
 		this.photocni = photocni;
 	}
+
+
 	public String getRelevebac() {
 		return relevebac;
 	}
+
+
 	public void setRelevebac(String relevebac) {
 		this.relevebac = relevebac;
 	}
+
+
 	public String getReleveproba() {
 		return releveproba;
 	}
+
+
 	public void setReleveproba(String releveproba) {
 		this.releveproba = releveproba;
 	}
+
+
 	public String getActenaiss() {
 		return actenaiss;
 	}
+
+
 	public void setActenaiss(String actenaiss) {
 		this.actenaiss = actenaiss;
 	}
+
+
 	public String getRecu() {
 		return recu;
 	}
+
+
 	public void setRecu(String recu) {
 		this.recu = recu;
 	}
 
-	public String getFaculte() {
-		return faculte;
-	}
-	public void setFaculte(String faculte) {
-		this.faculte = faculte;
-	}
-	public String getFiliere() {
-		return filiere;
-	}
-	public void setFiliere(String filiere) {
-		this.filiere = filiere;
-	}
-	public String getSpecialite() {
-		return specialite;
-	}
-	public void setSpecialite(String specialite) {
-
-		this.specialite = specialite;
-	}
-	public Niveau getNiveau() {
-		return niveau;
-	}
-	public void setNiveau(Niveau niveau) {
-		this.niveau = niveau;
-	}
-	public String getDernierdiplom() {
-		return dernierdiplom;
-	}
-	public void setDernierdiplom(String dernierdiplom) {
-		this.dernierdiplom = dernierdiplom;
-	}
-	public String getAnneeObtent() {
-		return anneeObtent;
-	}
-	public void setAnneeObtent(String anneeObtent) {
-		this.anneeObtent = anneeObtent;
-	}
-	public Double getMoyenne() {
-		return moyenne;
-	}
-	public void setMoyenne(Double moyenne) {
-		this.moyenne = moyenne;
-	}
-	public String getInfojury() {
-		return infojury;
-	}
-	public void setInfojury(String infojury) {
-		this.infojury = infojury;
-	}
-	public String getMatriculediplo() {
-		return matriculediplo;
-	}
-	public void setMatriculediplo(String matriculediplo) {
-		this.matriculediplo = matriculediplo;
-	}
-	public String getDelivrepar() {
-		return delivrepar;
-	}
-	public void setDelivrepar(String delivrepar) {
-		this.delivrepar = delivrepar;
-	}
-	public Date getDatedeliv() {
-		return Datedeliv;
-	}
-	public void setDatedeliv(Date datedeliv) {
-		Datedeliv = datedeliv;
-	}
-	public String getNompere() {
-		return nompere;
-	}
-	public void setNompere(String nompere) {
-		this.nompere = nompere;
-	}
-	public String getProfesspere() {
-		return professpere;
-	}
-	public void setProfesspere(String professpere) {
-		this.professpere = professpere;
-	}
-	public String getNommere() {
-		return nommere;
-	}
-	public void setNommere(String nommere) {
-		this.nommere = nommere;
-	}
-	public String getProfessmere() {
-		return professmere;
-	}
-	public void setProfessmere(String professmere) {
-		this.professmere = professmere;
-	}
-	public String getNomurgent() {
-		return nomurgent;
-	}
-	public void setNomurgent(String nomurgent) {
-		this.nomurgent = nomurgent;
-	}
-	public Double getNumerourgent() {
-		return numerourgent;
-	}
-	public void setNumerourgent(Double numerourgent) {
-		this.numerourgent = numerourgent;
-	}
-	public String getVilleurgent() {
-		return villeurgent;
-	}
-	public void setVilleurgent(String villeurgent) {
-		this.villeurgent = villeurgent;
-	}
-
-	@Override
-	public String toString() {
-		return "Student [photocni=" + photocni + ", relevebac=" + relevebac + ", releveproba=" + releveproba
-				+ ", actenaiss=" + actenaiss + ", recu=" + recu + ", faculte=" + faculte + ", filiere=" + filiere
-				+ ", specialite=" + specialite + ", niveau=" + niveau + ", dernierdiplom=" + dernierdiplom
-				+ ", anneeObtent=" + anneeObtent + ", moyenne=" + moyenne + ", infojury=" + infojury
-				+ ", matriculediplo=" + matriculediplo + ", delivrepar=" + delivrepar + ", Datedeliv=" + Datedeliv
-				+ ", nompere=" + nompere + ", professpere=" + professpere + ", nommere=" + nommere + ", professmere="
-				+ professmere + ", nomurgent=" + nomurgent + ", numerourgent=" + numerourgent + ", villeurgent="
-				+ villeurgent + "]";
-	}
-    
 
 	public String getPremierchoix() {
 		return premierchoix;
 	}
+
+
 	public void setPremierchoix(String premierchoix) {
 		this.premierchoix = premierchoix;
 	}
+
+
 	public String getDeuxiemechoix() {
 		return deuxiemechoix;
 	}
+
+
 	public void setDeuxiemechoix(String deuxiemechoix) {
 		this.deuxiemechoix = deuxiemechoix;
 	}
+
+
 	public String getTroisiemechoix() {
 		return troisiemechoix;
 	}
+
+
 	public void setTroisiemechoix(String troisiemechoix) {
 		this.troisiemechoix = troisiemechoix;
 	}
 
 
+	public String getSpecialite() {
+		return specialite;
+	}
+
+
+	public void setSpecialite(String specialite) {
+		this.specialite = specialite;
+	}
+
+
+	public Niveau getNiveau() {
+		return niveau;
+	}
+
+
+	public void setNiveau(Niveau niveau) {
+		this.niveau = niveau;
+	}
+
+
+	public String getDernierdiplom() {
+		return dernierdiplom;
+	}
+
+
+	public void setDernierdiplom(String dernierdiplom) {
+		this.dernierdiplom = dernierdiplom;
+	}
+
+
+	public String getAnneeObtent() {
+		return anneeObtent;
+	}
+
+
+	public void setAnneeObtent(String anneeObtent) {
+		this.anneeObtent = anneeObtent;
+	}
+
+
+	public Double getMoyenne() {
+		return moyenne;
+	}
+
+
+	public void setMoyenne(Double moyenne) {
+		this.moyenne = moyenne;
+	}
+
+
+	public String getInfojury() {
+		return infojury;
+	}
+
+
+	public void setInfojury(String infojury) {
+		this.infojury = infojury;
+	}
+
+
+	public String getMatriculediplo() {
+		return matriculediplo;
+	}
+
+
+	public void setMatriculediplo(String matriculediplo) {
+		this.matriculediplo = matriculediplo;
+	}
+
+
+	public String getDelivrepar() {
+		return delivrepar;
+	}
+
+
+	public void setDelivrepar(String delivrepar) {
+		this.delivrepar = delivrepar;
+	}
+
+
+	public Date getDatedeliv() {
+		return Datedeliv;
+	}
+
+
+	public void setDatedeliv(Date datedeliv) {
+		Datedeliv = datedeliv;
+	}
+
+
+	public String getNompere() {
+		return nompere;
+	}
+
+
+	public void setNompere(String nompere) {
+		this.nompere = nompere;
+	}
+
+
+	public String getProfesspere() {
+		return professpere;
+	}
+
+
+	public void setProfesspere(String professpere) {
+		this.professpere = professpere;
+	}
+
+
+	public String getNommere() {
+		return nommere;
+	}
+
+
+	public void setNommere(String nommere) {
+		this.nommere = nommere;
+	}
+
+
+	public String getProfessmere() {
+		return professmere;
+	}
+
+
+	public void setProfessmere(String professmere) {
+		this.professmere = professmere;
+	}
+
+
+	public String getNomtuteur() {
+		return nomtuteur;
+	}
+
+
+	public void setNomtuteur(String nomtuteur) {
+		this.nomtuteur = nomtuteur;
+	}
+
+
+	public String getEmailtuteur() {
+		return emailtuteur;
+	}
+
+
+	public void setEmailtuteur(String emailtuteur) {
+		this.emailtuteur = emailtuteur;
+	}
+
+
+	public String getProfesstuteur() {
+		return professtuteur;
+	}
+
+
+	public void setProfesstuteur(String professtuteur) {
+		this.professtuteur = professtuteur;
+	}
+
+
+	public String getNomurgent() {
+		return nomurgent;
+	}
+
+
+	public void setNomurgent(String nomurgent) {
+		this.nomurgent = nomurgent;
+	}
+
+
+	public Double getNumerourgent() {
+		return numerourgent;
+	}
+
+
+	public void setNumerourgent(Double numerourgent) {
+		this.numerourgent = numerourgent;
+	}
+
+
+	public String getVilleurgent() {
+		return villeurgent;
+	}
+
+
+	public void setVilleurgent(String villeurgent) {
+		this.villeurgent = villeurgent;
+	}
+
+
+	public int getEtat() {
+		return etat;
+	}
+
+
+	public void setEtat(int etat) {
+		this.etat = etat;
+	}
+
+
+	public Double getNumerotransaction() {
+		return numerotransaction;
+	}
+
+
+	public void setNumerotransaction(Double numerotransaction) {
+		this.numerotransaction = numerotransaction;
+	}
+
+
+	public Double getCodepreins() {
+		return codepreins;
+	}
+
+
+	public void setCodepreins(Double codepreins) {
+		this.codepreins = codepreins;
+	}
+
+
+	public boolean isSport() {
+		return sport;
+	}
+
+
+	public void setSport(boolean sport) {
+		this.sport = sport;
+	}
+
+
+	public boolean isArt() {
+		return art;
+	}
+
+
+	public void setArt(boolean art) {
+		this.art = art;
+	}
+
+
+	@Override
+	public String toString() {
+		return "Student [photocni=" + photocni + ", relevebac=" + relevebac + ", releveproba=" + releveproba
+				+ ", actenaiss=" + actenaiss + ", recu=" + recu + ", premierchoix=" + premierchoix + ", deuxiemechoix="
+				+ deuxiemechoix + ", troisiemechoix=" + troisiemechoix + ", specialite=" + specialite + ", niveau="
+				+ niveau + ", dernierdiplom=" + dernierdiplom + ", anneeObtent=" + anneeObtent + ", moyenne=" + moyenne
+				+ ", infojury=" + infojury + ", matriculediplo=" + matriculediplo + ", delivrepar=" + delivrepar
+				+ ", Datedeliv=" + Datedeliv + ", nompere=" + nompere + ", professpere=" + professpere + ", nommere="
+				+ nommere + ", professmere=" + professmere + ", nomtuteur=" + nomtuteur + ", emailtuteur=" + emailtuteur
+				+ ", professtuteur=" + professtuteur + ", nomurgent=" + nomurgent + ", numerourgent=" + numerourgent
+				+ ", villeurgent=" + villeurgent + ", etat=" + etat + ", numerotransaction=" + numerotransaction
+				+ ", codepreins=" + codepreins + ", sport=" + sport + ", art=" + art + "]";
+	}
 
 }
