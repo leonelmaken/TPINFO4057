dafe5ff899603a0113b0e0be5a0acf2fe0618b31
prunelle  ngolong <prunellengolong13@gmail.com>
2023-12-23 15:31:40 +0100

    mise  à jours
    modification

--- Back-end/Users/src/main/java/com/example/demo/models/InscriptionStudent.java
+++ Back-end/Users/src/main/java/com/example/demo/models/InscriptionStudent.java
@@ -10,20 +10,22 @@ import jakarta.persistence.ManyToOne;
 @Entity
 //@Data
 public class InscriptionStudent {
+	@Id
+	private int idins;
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 
-    @Id
-    private int idins;
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
+	@ManyToOne
+	private Student student;
 
-    @ManyToOne
-    private Student student;
+	private  String firsttranche;
+	private  String secondtranche;
+	private  String totalitetranche;
+	private  Double numtranche1;
+	private  Double numtranche2;
+	private  Double numTotalite;
 
-    private  String firsttranche;
-    private  String secondtranche;
-    private  String totalitetranche;
-    
 
-    public Student getStudent() {
+	public Student getStudent() {
 		return student;
 	}
 	public void setStudent(Student student) {
@@ -46,6 +48,64 @@ public class InscriptionStudent {
 	}
 	public void setTotalitetranche(String totalitetranche) {
 		this.totalitetranche = totalitetranche;
+	}
+
+
+
+	/**
+	 * @return int return the idins
+	 */
+	public int getIdins() {
+		return idins;
+	}
+
+	/**
+	 * @param idins the idins to set
+	 */
+	public void setIdins(int idins) {
+		this.idins = idins;
+	}
+
+	/**
+	 * @return Double return the numtranche1
+	 */
+	public Double getNumtranche1() {
+		return numtranche1;
+	}
+
+	/**
+	 * @param numtranche1 the numtranche1 to set
+	 */
+	public void setNumtranche1(Double numtranche1) {
+		this.numtranche1 = numtranche1;
+	}
+
+	/**
+	 * @return Double return the numtranche2
+	 */
+	public Double getNumtranche2() {
+		return numtranche2;
+	}
+
+	/**
+	 * @param numtranche2 the numtranche2 to set
+	 */
+	public void setNumtranche2(Double numtranche2) {
+		this.numtranche2 = numtranche2;
+	}
+
+	/**
+	 * @return Double return the numTotalite
+	 */
+	public Double getNumTotalite() {
+		return numTotalite;
+	}
+
+	/**
+	 * @param numTotalite the numTotalite to set
+	 */
+	public void setNumTotalite(Double numTotalite) {
+		this.numTotalite = numTotalite;
 	}
 
 }
