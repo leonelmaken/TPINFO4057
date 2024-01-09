2431d46998d8e9b2ac503bb15970ab307bf94e8c
leonelmaken <103751176+leonelmaken@users.noreply.github.com>
2024-01-09 19:23:30 +0100

    feign

--- Back-end/Communication/src/main/java/com/example/demo/controllers/AnnonceController.java
+++ Back-end/Communication/src/main/java/com/example/demo/controllers/AnnonceController.java
@@ -4,6 +4,7 @@ import com.example.demo.models.Annonce;
 import com.example.demo.service.AnnonceService;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
@@ -20,11 +21,11 @@ public class AnnonceController {
         this.annonceService = annonceService;
     }
 
-    @PostMapping(path = "/create")
-    public Annonce createAnnonce(@RequestPart("annonce") Annonce annonce, @RequestPart(value = "image", required = false) MultipartFile imageFile) {
+    @PostMapping(path = "/create", consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
+    public Annonce createAnnonce(@ModelAttribute Annonce annonce,
+                                 @RequestParam(value = "image", required = false) MultipartFile imageFile) {
         return annonceService.createAnnonce(annonce, imageFile);
     }
-
     @GetMapping(path = "/read")
     public List<Annonce> getAllAnnonces() {
         return annonceService.getAllAnnonces();
