64c4338092459c56c4ab041271fe0414d4a3219d
prunelle  ngolong <prunellengolong13@gmail.com>
2024-01-09 09:01:17 +0100

    ajout du teacher

--- Back-end/Users/src/main/java/com/example/demo/models/ImageUtils.java
+++ Back-end/Users/src/main/java/com/example/demo/models/ImageUtils.java
@@ -1,2 +1,14 @@
-package com.example.demo.models;public class ImageUtils {
+package com.example.demo.models;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.Base64;
+
+public class ImageUtils {
+
+    public static String encodeImageToBase64(String imagePath) throws IOException {
+        byte[] imageBytes = Files.readAllBytes(Paths.get(imagePath));
+        return Base64.getEncoder().encodeToString(imageBytes);
+    }
 }
